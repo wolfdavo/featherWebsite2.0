21f2438067eae78d00e7d6f9de73eec45fa97759
David Wolfenden <wolfdavo@gmail.com>
2019-04-05 13:55:37 -0500

    more testing

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,6 +1,6 @@
 <?php
 $errors = '';
-$myemail = 'wolfdavo@gmail.com';//<-----Put Your email address here.
+$myemail = 'david@featherconsulting.net';//<-----Put Your email address here.
 if(empty($_POST['Name'])  ||
    empty($_POST['EMail']) ||
    empty($_POST['Message']))
